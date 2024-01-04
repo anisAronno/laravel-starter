eeef32bbdba596c6be382a2feb4de2de92909cc7
Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2024-01-05 00:20:19 +0600

    Feature/media gallery (#23)
    * Change Media Width Ratio
    
    * Fix styling by bot
    
    ---------
    
    Co-authored-by: anisAronno <anisAronno@users.noreply.github.com>

--- resources/views/components/media/media.blade.php
+++ resources/views/components/media/media.blade.php
@@ -2,7 +2,7 @@
 @props(['visible'])
 @if ($visible == true)
     <div
-        class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 grow gap-5 p-5 bg-gray-100 dark:bg-gray-900">
+        class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 grow gap-5 p-5 bg-gray-100 dark:bg-gray-900">
         @foreach ($medias as $media)
             <div
                 class=" overflow-hidden h-full border  md:border-2 border-gray-300 dark:border-gray-700 rounded-sm bg-white dark:bg-gray-800">
