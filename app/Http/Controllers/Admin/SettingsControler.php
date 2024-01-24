df6af17549c162bd7d49eb7324b83636b0b3daf7
Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2024-01-24 22:07:13 +0600

    Complete Settings without Image Changes

---
+++ app/Http/Controllers/Admin/SettingsControler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use AnisAronno\LaravelSettings\Models\SettingsProperty;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SettingsControler extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('dashboard.settings.index', ['allSettings' => getAllSettings()]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $key)
+    {
+        $response = updateSettings($key, $request->settings_value);
+
+        if ($response) {
+            toast()->success('Best wishes', 'Settings updated successfully.');
+
+            return back();
+        }
+
+        toast()->error('Failed', 'Settings Updated Failed.');
+
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $key)
+    {
+        if (deleteSettings($key)) {
+            toast()->success('Best wishes', 'Settings deleted successfully.');
+
+            return back();
+        }
+
+        toast()->error('Failed', 'Settings Deleted Failed.');
+
+        return back();
+    }
+}
